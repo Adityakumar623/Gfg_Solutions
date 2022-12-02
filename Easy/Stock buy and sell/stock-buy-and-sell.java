80a59b52a2aa0d7097c2e7bf57ca2daf46df813b
Aditya kumar choubey <88607982+Adityakumar623@users.noreply.github.com>
2022-12-03 00:22:39 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Stock buy and sell/stock-buy-and-sell.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int A[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                A[i] = Integer.parseInt(inputLine[i]);
+            }
+            int p = 0;
+            for(int i=0; i<n-1; i++)
+                p += Math.max(0,A[i+1]-A[i]);
+                
+            Solution obj = new Solution();
+            ArrayList<ArrayList<Integer> > ans = obj.stockBuySell(A, n);
+            if(ans.size()==0)
+                System.out.print("No Profit");
+            else{
+                int c=0;
+                for(int i=0; i<ans.size(); i++)
+                    c += A[ans.get(i).get(1)]-A[ans.get(i).get(0)];
+                
+                if(c==p)
+                    System.out.print(1);
+                else
+                    System.out.print(0);
+            }System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    //Function to find the days of buying and selling stock for max profit.
+    ArrayList<ArrayList<Integer> > stockBuySell(int A[], int n) {
+       
+           // code here
+       ArrayList<ArrayList<Integer>>ans=new ArrayList<ArrayList<Integer>>();
+       for(int i=1;i<n;i++){
+           ArrayList<Integer>a1=new ArrayList<>();
+           if(A[i]>A[i-1]){
+               a1.add(i-1);
+               a1.add(i);
+               ans.add(a1);
+           }
+       }
+       return ans;
+    }
+}
