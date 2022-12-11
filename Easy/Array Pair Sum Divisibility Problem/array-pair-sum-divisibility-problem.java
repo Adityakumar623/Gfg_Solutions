26dd87763612c7c099787b2378c1bb9a57d66576
Aditya kumar choubey <88607982+Adityakumar623@users.noreply.github.com>
2022-12-11 19:56:39 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Array Pair Sum Divisibility Problem/array-pair-sum-divisibility-problem.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s1 = br.readLine().trim();
+            String[] S1 = s1.split(" ");
+            int n = Integer.parseInt(S1[0]);
+            int k = Integer.parseInt(S1[1]);
+            String s2 = br.readLine().trim();
+            String[] S2 = s2.split(" ");
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) {
+                nums[i] = Integer.parseInt(S2[i]);
+            }
+            Solution ob = new Solution();
+            boolean ans = ob.canPair(nums, k);
+            if (ans)
+                System.out.println("True");
+            else
+                System.out.println("False");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public boolean canPair(int[] nums, int k) {
+        // Code here
+       //int temp[]=new int[nums.length];
+       ArrayList<Integer> temp=new ArrayList<>();
+       int count=0;
+       
+       for(int i=0;i<nums.length;i++){
+           
+           if(nums[i]%k==0){
+               count++;
+           }
+           else{
+               temp.add(nums[i]%k);
+           }
+       }
+       Collections.sort(temp);
+       if(count%2==1)return false;
+       
+       int start=0;
+       int end=temp.size()-1;
+       
+       while(start<end){
+           if((temp.get(start)+temp.get(end))%k==0){
+               start++;
+               end--;
+           }
+           
+           else
+           return false;
+       }
+       
+       return true;
+       
+       
+    }
+}
