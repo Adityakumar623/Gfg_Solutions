2d8bf9b27dfcdfa00e5a6fdf95ff42a4f9197b0a
Aditya kumar choubey <88607982+Adityakumar623@users.noreply.github.com>
2023-01-24 00:15:41 +0530

    Updated Solution - GfG to GitHub

--- Medium/Geeks And The String/geeks-and-the-string.java
+++ Medium/Geeks And The String/geeks-and-the-string.java
@@ -27,17 +27,31 @@ class GFG {
 class Solution {
     public static String removePair(String s) {
         // code here
-        String ans = "";
-        Stack<Character> st = new Stack<>();
+        
+        Stack<Character> st =new Stack<>();
         
         for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(!st.isEmpty() && st.peek()==ch)   st.pop();
-            else    st.push(ch);
+            char ch=s.charAt(i);
+            
+            if(!st.isEmpty() && st.peek()==ch){
+                st.pop();
+            }
+            else{
+                st.push(ch);
+            }
         }
         
-        if(st.isEmpty())    return "-1";
-        while(!st.isEmpty())    ans = st.pop() + ans;
+        String ans="";
+        while(!st.isEmpty()){
+            ans=st.pop()+ans;
+        }
+        
+        if(ans==""){
+            return "-1";
+        }
+        
         return ans;
+        
     }
-}  
+}
+        
