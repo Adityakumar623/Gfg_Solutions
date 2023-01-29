eb6cf1c920ce814ca42c29cba56e24ab083100d0
Aditya kumar choubey <88607982+Adityakumar623@users.noreply.github.com>
2023-01-30 00:16:18 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximize Toys/maximize-toys.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String st[] = in.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(st[0]);
+            int K = Integer.parseInt(st[1]);
+            String st1[] = in.readLine().trim().split("\\s+");
+            int arr[] = new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(st1[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.toyCount(N, K, arr));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int toyCount(int N, int K, int arr[])
+    {
+        // code here
+        
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        
+        int count=0;
+        int amount=0;
+        // int x=0;
+        
+        for(int i=0;i<N;i++){
+         pq.add(arr[i]);
+        }
+        
+        while(!pq.isEmpty()){
+            amount+=pq.poll();
+            count++;
+            
+            if(amount==K){
+              return count;
+            }
+             
+            else if(amount>K){
+              return count-1;
+            }
+        }
+        
+       return count; 
+        
+    }
+}
