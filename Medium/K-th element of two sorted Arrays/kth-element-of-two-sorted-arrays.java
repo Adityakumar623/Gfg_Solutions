8a98d10551399f2d59dc282ff715d315f9df073a
Aditya kumar choubey <88607982+Adityakumar623@users.noreply.github.com>
2023-02-02 03:27:25 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/K-th element of two sorted Arrays/kth-element-of-two-sorted-arrays.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            int n = Integer.parseInt(stt.nextToken());
+            int m = Integer.parseInt(stt.nextToken());
+            int k = Integer.parseInt(stt.nextToken());
+            int a[] = new int[(int)(n)];
+            int b[] = new int[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(inputLine1[i]);
+            }
+            
+            
+            Solution obj = new Solution();
+            System.out.println(obj.kthElement( a, b, n, m, k));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    public long kthElement( int arr1[], int arr2[], int n, int m, int k) {
+        
+       ArrayList<Integer> al=new ArrayList<>();
+       
+       for(int i:arr1){
+           al.add(i);
+       }
+       for(int i:arr2){
+           al.add(i);
+       }
+       
+       Collections.sort(al);
+       
+       return (long)al.get(k-1);
+    }
+}
