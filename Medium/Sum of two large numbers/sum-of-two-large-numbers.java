2198a660f1239d1a31b2545cc44635a285c58fe0
Aditya kumar choubey <88607982+Adityakumar623@users.noreply.github.com>
2023-02-07 14:16:25 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of two large numbers/sum-of-two-large-numbers.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String x = read.readLine();
+            String y = read.readLine();
+            Solution ob = new Solution();
+            String result = ob.findSum(x, y);
+
+            System.out.println(result);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+   String findSum(String X, String Y) {
+       java.math.BigInteger a=new java.math.BigInteger(X);
+       java.math.BigInteger b=new java.math.BigInteger(Y);
+       
+       a=a.add(b);
+       return a.toString();
+   }
+}
