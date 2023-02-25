73e0c4f71e07481861c9205efa142f8fbe316c10
Aditya kumar choubey <88607982+Adityakumar623@users.noreply.github.com>
2023-02-26 01:57:59 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Search in a Rotated Array/search-in-a-rotated-array.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0)
+        {
+            int n = Integer.parseInt(in.readLine().trim());
+            int A[] = new int[n];
+            String s[]=in.readLine().trim().split(" ");
+            for (int i = 0;i < n;i++)
+            {
+                A[i] = Integer.parseInt(s[i]);
+            }
+            int key = Integer.parseInt(in.readLine().trim());
+            
+            out.println(new Solution().search(A, 0, n - 1, key));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    int search(int A[], int l, int h, int key)
+    {
+        // l: The starting index
+        // h: The ending index, you have to search the key in this range
+        // Complete this function
+        
+        for(int i=0;i<A.length;i++){
+            if(A[i]==key){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
