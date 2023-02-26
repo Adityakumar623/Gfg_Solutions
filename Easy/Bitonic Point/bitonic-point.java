94452563cfc8cb3d6f3b40c6acc6bbfb4853fb06
Aditya kumar choubey <88607982+Adityakumar623@users.noreply.github.com>
2023-02-26 22:12:57 +0530

    Updated Solution - GfG to GitHub

--- Easy/Bitonic Point/bitonic-point.java
+++ Easy/Bitonic Point/bitonic-point.java
@@ -35,7 +35,47 @@ public class Main {
 class Solution {
     int findMaximum(int[] arr, int n) {
         // code here
-       Arrays.sort(arr);
-       return arr[n-1];
+      //  it will made using Binary search or it is very similar to finding peak element
+      
+      int low=0; int high=n-1;
+      
+      while(low<=high){
+          int mid=low+(high-low)/2;
+          
+          if(mid>0 && mid<n-1){
+              if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                  return arr[mid];
+              }
+              
+              else if(arr[mid]<arr[mid-1]){
+                  high=mid-1;
+              }
+              
+              else if(arr[mid]<arr[mid+1]){
+                  low=mid+1;
+              }
+          }
+          
+          else if(mid==0){
+              if(arr[mid]>arr[mid+1]){
+                  return arr[mid];
+              }
+              else{
+                  return arr[1];
+              }
+          }
+          
+          else if(mid==n-1){
+              if(arr[mid]>arr[mid-1]){
+                  return arr[mid];
+              }
+              
+              else{
+                  return arr[n-2];
+              }
+          }
+      }
+      
+      return -1;
     }
 }
