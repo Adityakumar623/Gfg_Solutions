724e4db90c96bb748789a8b53059df6a9b9a5bcd
Aditya kumar choubey <88607982+Adityakumar623@users.noreply.github.com>
2023-04-25 20:26:36 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum of all subarrays of size k/maximum-of-all-subarrays-of-size-k.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])
+    {
+        //taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total count of testcases
+        int t = sc.nextInt();
+        
+        
+        
+        while(t-- > 0)
+        {
+            //taking total number of elements
+            int n = sc.nextInt();
+            
+            //taking size of subArrays 
+            int k = sc.nextInt();
+            
+            //Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+            
+            //adding all the elements to the array 
+            for(int i = 0; i <n; i++)
+            {
+                arr[i] =sc.nextInt();
+            }
+            
+            //Calling the method max_of_subarrays of class solve
+            //and storing the result in an ArrayList
+            ArrayList <Integer> res = new Solution().max_of_subarrays(arr, n, k);
+            
+            //printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print (res.get (i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to find maximum of each subarray of size k.
+   static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
+    {
+        // Your code here
+        int left=0;
+        ArrayList<Integer>al=new ArrayList<>();
+       for(int i=k-1; i<arr.length;i++)
+       {
+           int ans=returnMax(arr,left,i);
+           al.add(ans);
+           left++;
+       }
+       return al;
+    }
+  static int returnMax(int []arr,int startindex,int k)
+  { int max=Integer.MIN_VALUE;
+      for(int i=startindex;i<=k;i++)
+      {
+          if(arr[i]>max)
+              max=arr[i];
+      }
+      
+      return max;
+  }
+}
